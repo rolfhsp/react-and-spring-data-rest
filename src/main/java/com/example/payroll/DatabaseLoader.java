455dd7804c501ee7e6f36ecd010715a33ff55631
Rolf Pedersen <rolf.pedersen@spk.no>
2018-04-25 16:58:05 +0200

    Add DatabaseLoader class from CommandLineRunner.

---
+++ src/main/java/com/example/payroll/DatabaseLoader.java
@@ -0,0 +1,19 @@
+package com.example.payroll;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+public class DatabaseLoader implements CommandLineRunner {
+
+    private final EmployeeRepository repository;
+
+    @Autowired
+    public DatabaseLoader(EmployeeRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        this.repository.save(new Employee("Frodo", "Baggins", "ring bearer"));
+    }
+}
